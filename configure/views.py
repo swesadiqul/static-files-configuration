a6459ce093947eb056f5f7f893190e9ccc0d6fcf
swesadiqul <mdsadiqulislam390@gmail.com>
2022-12-27 18:02:25 +0600

    bootstrap form

--- configure/views.py
+++ configure/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
+    if request.method == "GET":
+        email = request.GET.get('email')
+        password = request.GET.get('password')
+        check = request.GET.get('check')
+        print(email)
+        print(password)
+        print(check)
     return render(request, template_name="index.html")
